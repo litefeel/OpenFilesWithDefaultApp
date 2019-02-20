f89fd3458b2cf9be68a518cf266b0f209dc5c547
litefeel <litefeel@gmail.com>
2019-02-20 20:10:53 +0800

    Support Preferences Setting

--- Assets/Plugins/Litefeel/OpenFileWithDefaultApp/Editor/OpenFileWithDefaultApp.cs
+++ Assets/Plugins/Litefeel/OpenFileWithDefaultApp/Editor/OpenFileWithDefaultApp.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace litefeel.OpenFileWithDefaultApp
@@ -8,6 +11,25 @@ namespace litefeel.OpenFileWithDefaultApp
     public class OpenFileWithDefaultApp
     {
 
+        [OnOpenAsset(0)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            var exts = PreferencesGUI.FileExts;
+            if (exts.Length == 0) return false;
+
+            string assetPath = AssetDatabase.GetAssetPath(instanceID);
+
+            foreach(var ext in exts)
+            {
+                if(assetPath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
+                {
+                    EditorUtility.OpenWithDefaultApp(assetPath);
+                    return true;
+                }
+            }
+            
+            return false;
+        }
         
     }
 }
