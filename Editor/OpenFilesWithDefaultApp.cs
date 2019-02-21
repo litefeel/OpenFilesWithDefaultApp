553290347e609f4cb7d950988f58961b8b752c48
litefeel <litefeel@gmail.com>
2019-02-22 01:31:43 +0800

    Add upm files

---
+++ Editor/OpenFilesWithDefaultApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace litefeel.OpenFilesWithDefaultApp
+{
+
+    public static class OpenFilesWithDefaultApp
+    {
+
+        [OnOpenAsset(0)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            var exts = PreferencesGUI.FileExts;
+            if (exts.Length == 0) return false;
+
+            string assetPath = AssetDatabase.GetAssetPath(instanceID);
+
+            foreach(var ext in exts)
+            {
+                if(assetPath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
+                {
+                    EditorUtility.OpenWithDefaultApp(assetPath);
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Open Files With Default App
+
+Open Files With Default App is just perfect Unity asset plugin to open file with system default Application.
+
+#### Feature list:
+
+* Open files with default application
+* No runtime resources required
+* No scripting required
+
+##### How to use?
+
+1. Import this plugin from?Asset Store
+2. Open Windows from [Edit] > [Preferences…] > [Open Files]
+3. Input the file extensions, like txt
+4. Double click txt file, will open it with default application
+
+
+Support
+* Create issues by issues page (https://github.com/litefeel/OpenFilesWithDefaultApp/issues)
+* Send email to me: litefeel@gmail.com
