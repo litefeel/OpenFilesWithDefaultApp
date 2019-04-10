4be2842db25361ffdbec2edc18d5a6d52a75d8d8
litefeel <litefeel@gmail.com>
2019-04-10 22:59:48 +0800

    Support for opening package files

--- Editor/OpenFilesWithDefaultApp.cs
+++ Editor/OpenFilesWithDefaultApp.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 namespace litefeel.OpenFilesWithDefaultApp
@@ -10,6 +12,8 @@ namespace litefeel.OpenFilesWithDefaultApp
 
     public static class OpenFilesWithDefaultApp
     {
+        private const string PACKAGES = "Packages/";
+        private const string ASSETS = "Assets/";
 
         [OnOpenAsset(0)]
         public static bool OnOpenAsset(int instanceID, int line)
@@ -17,19 +21,52 @@ namespace litefeel.OpenFilesWithDefaultApp
             var exts = PreferencesGUI.FileExts;
             if (exts.Length == 0) return false;
 
+            // Packages/com.unity.render-pipelines.lightweight/Shaders/Unlit.shader
             string assetPath = AssetDatabase.GetAssetPath(instanceID);
 
-            foreach(var ext in exts)
+            foreach (var ext in exts)
             {
-                if(assetPath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
+                if (assetPath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
                 {
-                    EditorUtility.OpenWithDefaultApp(assetPath);
+                    if (assetPath.StartsWith(ASSETS))
+                        EditorUtility.OpenWithDefaultApp(assetPath);
+                    else if (assetPath.StartsWith(PACKAGES))
+                        OpenByPackages(assetPath);
                     return true;
                 }
             }
-            
+
             return false;
         }
-        
+
+        private static void OpenByPackages(string assetPath)
+        {
+            var request = Client.List(true);
+            EditorApplication.CallbackFunction update = null;
+
+            update = () =>
+            {
+                if (!request.IsCompleted) return;
+                EditorApplication.update -= update;
+                if (request.Status != StatusCode.Success) return;
+
+                foreach (var entity in request.Result)
+                {
+                    // resolvedPath D:\work\MyProject\Library\PackageCache\com.unity.render-pipelines.lightweight@4.10.0-preview
+                    if (assetPath.StartsWith(entity.assetPath))
+                    {
+                        var absPath = Path.Combine(entity.resolvedPath, GetPathWithoutPackage(assetPath));
+                        EditorUtility.OpenWithDefaultApp(absPath);
+                    }
+                }
+            };
+            EditorApplication.update += update;
+        }
+
+        private static string GetPathWithoutPackage(string packagePath)
+        {
+            var index = packagePath.IndexOf('/', PACKAGES.Length);
+            return packagePath.Substring(index + 1);
+        }
     }
 }
