cea46ca34be6d0d919761335576d9d4318201066
litefeel <litefeel@gmail.com>
2019-05-01 09:03:31 +0800

    Simplified logic

--- Editor/OpenFilesWithDefaultApp.cs
+++ Editor/OpenFilesWithDefaultApp.cs
@@ -28,47 +28,13 @@ namespace litefeel.OpenFilesWithDefaultApp
             {
                 if (assetPath.EndsWith(ext, StringComparison.OrdinalIgnoreCase))
                 {
-                    if (assetPath.StartsWith(ASSETS))
-                        EditorUtility.OpenWithDefaultApp(assetPath);
-                    else if (assetPath.StartsWith(PACKAGES))
-                        OpenByPackages(assetPath);
+                    var fullPath = Path.GetFullPath(assetPath);
+                    EditorUtility.OpenWithDefaultApp(fullPath);
                     return true;
                 }
             }
 
             return false;
-        }
-
-        private static void OpenByPackages(string assetPath)
-        {
-#if UNITY_2018_1_OR_NEWER
-            var request = Client.List(true);
-            EditorApplication.CallbackFunction update = null;
-
-            update = () =>
-            {
-                if (!request.IsCompleted) return;
-                EditorApplication.update -= update;
-                if (request.Status != StatusCode.Success) return;
-
-                foreach (var entity in request.Result)
-                {
-                    // resolvedPath D:\work\MyProject\Library\PackageCache\com.unity.render-pipelines.lightweight@4.10.0-preview
-                    if (assetPath.StartsWith(entity.assetPath))
-                    {
-                        var absPath = Path.Combine(entity.resolvedPath, GetPathWithoutPackage(assetPath));
-                        EditorUtility.OpenWithDefaultApp(absPath);
-                    }
-                }
-            };
-            EditorApplication.update += update;
-#endif
-        }
-
-        private static string GetPathWithoutPackage(string packagePath)
-        {
-            var index = packagePath.IndexOf('/', PACKAGES.Length);
-            return packagePath.Substring(index + 1);
         }
     }
 }
